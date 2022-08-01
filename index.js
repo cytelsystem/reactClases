a882302e5d26458cf409e1c550c6abad333c7542
Hector Javier Moreno <cytelsystem@gmail.com>
2022-08-01 18:56:42 -0500

    clase1

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 
-  const App = React.createElement('h1', null, 'Hola Mundo!');
+
+  // const App = React.createElement('h1', null, 'Hola Mundo!');
+
+  const App = React.createElement("div",null, [
+
+      React.createElement("h1", {key: 1}, "Con lo visto hasta ahora"),
+      React.createElement("h2", {key: 2}, "Replica este HTML"),
+      React.createElement("h3", {key: 3}, "Usando"),
+      React.createElement("h4", {key: 4}, "React.js"),
+      React.createElement("footer", {key: 5}, "Suerte!")
+
+  ]
+  );
+
 
   const contenedor = document.getElementById('root');
 
-  ReactDOM.render(App, contenedor);
+  const root = ReactDOM.createRoot(contenedor);
+  root.render(App);
+
+
+
 
