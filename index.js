f10c1ebf8e425dcb90c18d4ba49a0f553c502663
Hector Javier Moreno <cytelsystem@gmail.com>
2022-08-03 18:05:10 -0500

    clase2 funcionando

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 
 
-  // const App = React.createElement('h1', null, 'Hola Mundo!');
+function App() {
+  return (
+    <div>
+      <h1>Con lo visto hasta ahora</h1>
+      <h2>Replica este HTML</h2>
+      <h3>Usando</h3>
+      <h4>React.js</h4>
+      <footer>Suerte!</footer>
+    </div>
+  )
+}
 
-  const App = React.createElement("div",null, [
 
-      React.createElement("h1", {key: 1}, "Con lo visto hasta ahora"),
-      React.createElement("h2", {key: 2}, "Replica este HTML"),
-      React.createElement("h3", {key: 3}, "Usando"),
-      React.createElement("h4", {key: 4}, "React.js"),
-      React.createElement("footer", {key: 5}, "Suerte!")
+const contenedor = document.getElementById('root');
 
-  ]
-  );
-
-
-  const contenedor = document.getElementById('root');
-
-  const root = ReactDOM.createRoot(contenedor);
-  root.render(App);
+const root = ReactDOM.createRoot(contenedor);
+root.render(<App />);
 
 
 
