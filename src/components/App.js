0522ae988b29b9890f94da4bab99b972b6575861
Hector Javier Moreno <cytelsystem@gmail.com>
2022-08-05 21:51:38 -0500

    Fragment funcionando

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 function App() {
+
+  const numeros = [1, 2, 3, 4, 5, 6, 7, 8];
+
   return (
-    <div> <h1>App js</h1></div>
+    <>
+      {numeros.map(numero => (
+        <React.Fragment key={numero}>
+          <span>No</span>
+          <p>{numero}</p>
+        </React.Fragment>
+      ))}
+    </>
+
   )
+
 }
 
 export default App;
