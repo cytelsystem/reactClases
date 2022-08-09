b56792bbe3a8e5e3b0695ce86a3b21fdbc199ce7
Hector Javier Moreno <cytelsystem@gmail.com>
2022-08-08 21:20:10 -0500

    funcionando

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,46 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import datosJson from '../configuracion.json'
 
 
-const url = "https://benjumeacarlos981.github.io/multicarga/configuracion.json";
-
-const datos1 = "";
-
-function prueba(datos){
-  datos1 = datos;
-
-}
-
-fetch(url)
-.then(respuesta => {
-  return respuesta.json()
-
-})
-.then(datos => {
-
-  prueba(datos);
-
-})
+// const url = "https://randomuser.me/api/";
 
 
-function App () {
 
-  console.log(datos1)
-
-
-  return (
-    <h1>prueba</h1>
+function App (datos) {
 
 
-  )
+  const url = "https://benjumeacarlos981.github.io/multicarga/original.json";
+  const [configuracion, setTodos] = useState()
+
+  const fetchApi = async () => {
+    const response = await fetch(url)
+    const responseJSON = await response.json()
+    console.log(responseJSON)
+    setTodos(responseJSON)
+  }
+
+  useEffect(() => {
+    fetchApi();
+
+  }, [])
+
+
+
+
+    return (
+      <div>
+        <ul>
+          { !configuracion ? 'Cargando...' :
+          <li key="1">{configuracion.contacto.telefono}</li>
+
+
+          }
+        </ul>
+
+
+    </div>
+
+    )
 
 
 
