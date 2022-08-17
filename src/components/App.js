b4a4dea915c5b5b9b2d07e535c7d55b2a245df11
Hector Javier Moreno <cytelsystem@gmail.com>
2022-08-17 17:54:52 -0500

    dfsadadsa

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,51 @@
-import React from 'react';
+import './styles/app.css'
+import React, { Component, useState } from 'react'
+import Button from './Button.js'
+import "./styles/app.css";
+
+  /* ESPACIO DE TRABAJO
+  - Armar la estructura para una Class Component
+  - Hacer un state con el arreglo de los colores
+  - Vamos a usar una funcion shuffle mas abajo y vamos a enviarla por props... que
+    formas tenemos para no perder el contexto?
+  */
+
+
+  /* ESPACIO DE TRABAJO
+  Funcion Shuffle
+  Aqui deberan hacer una funcion que me retorne un nuevo arreglo con los
+  colores mezclados
+  */
 
 function App() {
+
+  const colors = ["RED","BLUE","YELLOW", "GREEN","ORANGE","MAGENTA","BROWN","LIME"]
+
+  const [Colores, setColores] = useState(colors)
+
+
+function shuffle(array) {
+  return array.sort(() => Math.random() - 0.5);
+}
+
+function cambiarColores() {
+  setColores(shuffle(colors))
+
+}
+
+
   return (
-    <div> <h1>App js</h1></div>
+
+    <div className="container">
+        <div className="panel">
+          {Colores.map(color =>
+          <div key={color} className={color}>{color}</div>
+          )}
+        </div>
+        <Button fn={cambiarColores} />
+
+    </div>
+
   )
 }
 
