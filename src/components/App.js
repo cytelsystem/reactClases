699b4731cfafb337aff9d1b41833a1054b9d8e6d
Hector Javier Moreno <cytelsystem@gmail.com>
2022-08-22 11:33:07 -0500

    cancelar pedido

--- src/components/App.js
+++ src/components/App.js
@@ -3,25 +3,47 @@ import React from 'react';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {imHungryFor: "Empanadas"};
+    this.state = {
+                    imHungryFor: "",
+                    show: true
+                 };
   }
+
+  cancelOrder = () => {
+    this.setState({show: false});
+  }
+
   componentDidMount() {
     setTimeout(() => {
-      this.setState({imHungryFor: "Pizza"})
-    }, 1000)
+      this.setState({imHungryFor: "Pizzas"})
+    }, 2000)
   }
   componentDidUpdate() {
-    document.getElementById("myH2").innerHTML =
-    "Ahora tengo ganas de comer " + this.state.imHungryFor;
     console.log("El componente se actualizó!");
   }
   render() {
+    let myOrder;
+    if (this.state.show) {
+      myOrder = <Child food={this.state.imHungryFor}/>;
+    };
     return (
       <div>
-      <h1>Tengo ganas de comer {this.state.imHungryFor}</h1>
-      <h2 id="myH2"></h2>
+      {myOrder}
+      <button type="button" onClick={this.cancelOrder}>Cancelar pedido.</button>
       </div>
     );
   }
 }
+
+class Child extends React.Component {
+  componentWillUnmount() {
+    alert("Tu pedido ha sido cancelado.");
+  }
+  render() {
+    return (
+      <h1>Tu pedido: {this.props.food}</h1>
+    );
+  }
+}
+
 export default App;
