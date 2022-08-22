12c34784f31469290c89facbdbe013cb948db01f
Hector Javier Moreno <cytelsystem@gmail.com>
2022-08-22 11:31:49 -0500

    comer pizza

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {imHungryFor: "Empanadas"};
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({imHungryFor: "Pizza"})
+    }, 1000)
+  }
+  componentDidUpdate() {
+    document.getElementById("myH2").innerHTML =
+    "Ahora tengo ganas de comer " + this.state.imHungryFor;
+    console.log("El componente se actualizó!");
+  }
   render() {
     return (
-      <h1>prueba</h1>
-    )
+      <div>
+      <h1>Tengo ganas de comer {this.state.imHungryFor}</h1>
+      <h2 id="myH2"></h2>
+      </div>
+    );
   }
 }
-
 export default App;
