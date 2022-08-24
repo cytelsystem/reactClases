8b7d8eb775150ee8071c09f39dbe4cdc04d20730
Hector Javier Moreno <cytelsystem@gmail.com>
2022-08-24 11:01:39 -0500

    fsdafsadfa

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react'
 import {pokeContainer, pokeList, pokeOption} from "./styles/style"
-export default class Pokedex extends Component {
+
+class Pokedex extends Component {
+
 
     render() {
         const {updateParent} = this.props
+        console.log(updateParent);
         return (
             <div style={pokeContainer}>
                 <h3>POKEDEX</h3>
                 <div style={pokeList}>
+
                     {this.props.pokemons.map(pokemon => (
-                    <p onClick={() => updateParent({pokemon: [pokemon.name, pokemon.id, pokemon.type]})} style={pokeOption}>{pokemon.name}</p>
+
+                    <p onClick={() => updateParent({pokemon: [pokemon.name, pokemon.id, pokemon.type]})} style={pokeOption}>
+                        {pokemon.name}
+                    </p>
+
                     ))}
+
                 </div>
             </div>
         )
     }
 }
+
+export default Pokedex;
