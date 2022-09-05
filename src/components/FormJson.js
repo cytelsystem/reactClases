2c0bfae4e110a3ff10692f5fa22d24da6deda978
Hector Javier Moreno <cytelsystem@gmail.com>
2022-09-05 11:34:47 -0500

    funcionando con botones

--- src/components/FormJson.js
+++ src/components/FormJson.js
@@ -1,10 +1,39 @@
-
+import { useState } from 'react'
+import { saveAs } from 'file-saver';
+import styles from './styles/formjson.module.scss';
 const Form = ({ handleSubmit, handleChange }) => {
+
+  const [link1, setLink1] = useState("none");
+  const [link2, setLink2] = useState("none");
+
+  function getNewFileHandle(data) {
+
+    const opts = {
+      excludeAcceptAllOption: true ,
+      suggestedName: data,
+      types: [{
+        description: 'Text file',
+        // accept: {
+        //   'text/plain': ['.txt']
+        // },
+        accept: {
+          'image/*': ['.jpg', '.txt']
+        }
+
+      }],
+    };
+
+    const prueba = window.showSaveFilePicker(opts);
+
+    console.log(prueba)
+    return prueba;
+  }
+
 
 
   return (
         <>
-          <form onSubmit={handleSubmit}>
+          <form onSubmit={handleSubmit} className={styles.form}>
             <div className="form-header">
               <h2>Guardar jSon</h2>
             </div>
@@ -19,16 +48,86 @@ const Form = ({ handleSubmit, handleChange }) => {
                 />
               </div>
             <div className="form-group">
-                <label>Nombrelink</label>
+                <label>Detalle</label>
                 <input
                   onChange={handleChange}
                   className="form-control"
                   type="text"
-                  name="nombreLink"
+                  name="detalle"
                 />
+            </div>
+
+            <div style={{display: `${link1}`}} className={`${styles.link} ${styles.link1}`}>
+              <div className="form-group">
+                  <label>Link Nombre1</label>
+                  <input
+                    onChange={handleChange}
+                    className="form-control"
+                    type="text"
+                    name="link1"
+                  />
               </div>
+              <div className="form-group">
+                  <label>Link Archivo1</label>
+                  <input
+                    id="archivo1"
+                    onChange={handleChange}
+                    className="form-control"
+                    type="text"
+                    name="archivo1"
+                  />
+                  <button onClick={() => {
+                    var valor = document.getElementById("archivo1").value;
+                    return getNewFileHandle(valor)
+                    }} >archivos1</button>
+              </div>
+            </div>
+
+
+            <div style={{display: `${link2}`}} className={`${styles.link} ${styles.link2}`}>
+              <div className="form-group">
+                  <label>Link Nombre2</label>
+                  <input
+                    onChange={handleChange}
+                    className="form-control"
+                    type="text"
+                    name="link2"
+                  />
+              </div>
+              <div className="form-group">
+                  <label>Archivo 2</label>
+                  <input
+                    onChange={handleChange}
+                    className="form-control"
+                    type="text"
+                    name="archivo2"
+                  />
+                  <button onClick={() => {
+                    var valor = document.getElementById("archivo1").value;
+                    return getNewFileHandle(valor)
+                  }} >archivos1</button>
+              </div>
+            </div>
+
+
+
 
             <button type="submit">Registrar Usuario</button>
+            <button onClick={() => (getNewFileHandle("javier.txt"))} >archivos1</button>
+            <button onClick={() => {
+                   {link1 === "none" ? setLink1("block") : link2 === "none" ? setLink2("block") : setLink2("block")}
+                  }
+              }>
+                +
+            </button>
+
+            <button onClick={() => {
+                   {link2 === "block" ? setLink2("none") : link1 === "block" ? setLink1("none") : setLink1("block")}
+                  }
+              }>
+                -
+            </button>
+
           </form>
         </>
       )
--- src/pages/FcGuardarDatos.js
+++ src/pages/FcGuardarDatos.js
@@ -1,21 +1,27 @@
 
-import { useState } from 'react';
-import api from '../Api'; // importar api.js para hacer una llamada a la api
+import { useState, useEffect } from 'react';
+import api from '../api';
 import FormJson from '../components/FormJson';
-
-
 
 const FcGuardarDatos = () => {
 
-  const [nombre, setNombre] = useState('')
-  const [nombreLink, setNombreLink] = useState('')
+  const [nombrecompleto, setNombre] = useState('')
+  const [detalle, setDetalle] = useState('')
+  const [link1, setLink1] = useState('')
+  const [archivo1, setArchivo1] = useState('')
+  const [link2, setLink2] = useState('')
+  const [archivo2, setArchivo2] = useState('')
+
+  const linkArray = [];
+
+
+
+
 
 
   const handleChange = (e) => {
     const setter = getSetter(e.target.name)
     setter(e.target.value)
-    const valid = [nombre, nombreLink].every(v => v.length > 0)
-
   };
 
 
@@ -25,14 +31,27 @@ const FcGuardarDatos = () => {
 
     const payload = {
       id: '',
-      nombre,
+      nombrecompleto,
+      detalle,
       link: [
-        {
-          nombreLink
-        }
-      ]
 
+
+      ]
     }
+
+
+    if (link1){
+      const nombre = link1;
+      const archivo = archivo1;
+      {payload.link.push({nombre, archivo})}
+    }
+    if (link2){
+      const nombre = link2;
+      const archivo = archivo2;
+      {payload.link.push({nombre, archivo})}
+    }
+
+
 
     await api.prueba.create(payload);
 
@@ -42,7 +61,11 @@ const FcGuardarDatos = () => {
   const getSetter = function (controlName) {
     switch (controlName) {
       case 'nombre': return setNombre
-      case 'nombreLink': return setNombreLink
+      case 'detalle': return setDetalle
+      case 'link1': return setLink1
+      case 'archivo1': return setArchivo1
+      case 'link2': return setLink2
+      case 'archivo2': return setArchivo2
       default: return () => {}
     }
   }
@@ -51,7 +74,6 @@ const FcGuardarDatos = () => {
     return (
       <>
           <FormJson handleSubmit={handleSubmit} handleChange={handleChange} />
-
       </>
     );
 
